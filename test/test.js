6499daf2b8c107a42d554dffb502f6006fa00d74
Mark Stewart <mark@grit-design.com>
2013-11-25 00:12:43 -0500

    linting, gruntfile, working

--- test/test.js
+++ test/test.js
@@ -2,32 +2,27 @@ var Context = require("../lib/fad").Context;
 
 var ctx = new Context();
 
-/*
 ctx.set("projectId", "11111");
 
-ctx.add([
-function anotherKey(cb) {
-    setTimeout(function() {
-	cb(null, "another");
-    }, 1000); 
-},
-function rootKey(cb) {
-    setTimeout(function() {
-	cb(null, "/this/is/a/test");
-    }, 1000); 
-}
-]);
+ctx.addRule(function anotherKey(cb) {
+    cb(null, "another");
+});
+
+ctx.addRule(function rootKey(cb) {
+    console.log("!!!! " + cb);
+    cb(null, "/this/is/a/test");
+});
 
 ctx.get(function(rootKey, anotherKey, projectId) {
     console.log("rootKey is " + rootKey);
     console.log("another key  is " + anotherKey);
     console.log("projectId" + projectId);
 
-    ctx.get(function(rootKey, anotherKey, projectId) {
+    ctx.get(["rootKey", "anotherKey", "projectId"], function(err, results) {
     console.log("\nwas that  quick?");
-    console.log("rootKey is " + rootKey);
-    console.log("another key  is " + anotherKey);
-    console.log("projectId" + projectId);
+    console.log("rootKey is " + results[0]);
+    console.log("another key  is " + results[1]);
+    console.log("projectId" + results[2]);
     });
 
     console.log("THIS SHOULD BE BEFORE the end");
@@ -50,4 +45,3 @@ ctx.get(function(rootKey, anotherKey, projectId) {
 
 });
 
-*/
