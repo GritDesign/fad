2264df3b43ae0bd8e89ad7a2313c2f0d9971db58
Mark Stewart <mark@grit-design.com>
2013-11-27 01:09:16 -0500

    tests and various fixes

--- spec/fad-spec.js
+++ spec/fad-spec.js
@@ -33,9 +33,7 @@ describe("create()", function () {
 
 	it("should not allow rules without names", function () {
 		var rules = [
-			function (x, y, cb) {
-				cb(null, x + y);
-			}
+			function () {}
 		];
 		expect(function () {
 			fad.create(rules);
@@ -102,7 +100,7 @@ describe("ctx.get", function () {
 		function async10(cb) {
 			setTimeout(function () {
 				cb(null, 10);
-			}, 20);
+			}, 200);
 		},
 		function addResult(a, b, async10, cb) {
 			cb(null, a + b + async10);
@@ -114,7 +112,10 @@ describe("ctx.get", function () {
 			cb(new Error("2nd error"));
 		},
 		function dependsOnErrorRule(errorRule, cb) {
-			cb(null, "no problem");
+			cb(null, "should have been error!");
+		},
+		function dependsOnNothing(nothing, cb) {
+			cb(null, "should have been error!");
 		}
 	];
 
@@ -122,6 +123,22 @@ describe("ctx.get", function () {
 
 	ctx.set("a", 10);
 	ctx.set("b", 20);
+
+	it("should propagate errors in the rules", function (done) {
+		ctx.get(function (err, errorRule) {
+			expect(errorRule).toBeNull();
+			expect(err).toEqual(new Error("There was an error"));
+			done();
+		});
+	});
+
+	it("should propagate errors in the rule dependencies", function (done) {
+		ctx.get(function (err, dependsOnErrorRule) {
+			expect(err).toEqual(new Error("There was an error"));
+			expect(dependsOnErrorRule).toBeNull();
+			done();
+		});
+	});
 
 	it("should allow a single function argument", function (done) {
 		ctx.get(function (a, b) {
@@ -149,10 +166,17 @@ describe("ctx.get", function () {
 		});
 	});
 
-	it("should not allow being called with (number)", function () {
-		expect(function () {
-			ctx.get(3);
-		}).toThrow(new Error("invalid arguments to get()"));
+	it("should not allow being called with (number)", function (done) {
+		try {
+			ctx.get(3, function (err) {
+				expect(err).toEqual(new Error("invalid arguments to get()"));
+				done();
+			});
+		} catch (e) {
+			expect(e).toEqual(new Error("invalid arguments to get()"));
+			done();
+		}
+		
 	});
 
 	it("should not call callback before returning", function (done) {
@@ -179,13 +203,45 @@ describe("ctx.get", function () {
 	});
 
 	it("should work with asynchronous rules (3rd time)", function (done) {
-		ctx.get(function (addResult) {
+		var ctx3 = fad.create(rules);
+		ctx3.set("a", 10);
+		ctx3.set("b", 20);
+		ctx3.get(function (addResult) {
 			expect(addResult).toEqual(40);
-			ctx.get(function (addResult) {
+			ctx3.get(function (addResult) {
 				expect(addResult).toEqual(40);
 				done();
 			});
 		});
+	});
+
+	it("should call functions only a single time no deps", function (done) {
+		var callCount = 0;
+		var count = 2;
+		var ctx3 = fad.create([
+			function waitForIt(cb) {
+				setTimeout(function () {
+					callCount++;
+					cb(null, 10);
+				}, 100);
+			}
+		]);
+		ctx3.get(function (waitForIt) {
+			expect(waitForIt).toEqual(10);
+			count--;
+			checkDone();
+		});
+		ctx3.get(function (waitForIt) {
+			expect(waitForIt).toEqual(10);
+			count--;
+			checkDone();
+		});
+		function checkDone() {
+			if (count === 0) {
+				expect(callCount).toEqual(1);
+				done();
+			}
+		}
 	});
 
 	it("should ignore rules that don't have all dependencies", function (done) {
@@ -197,25 +253,9 @@ describe("ctx.get", function () {
 
 	it("should not resolve properties that don't exist", function (done) {
 		ctx.get(function (err, nothing) {
-			expect(nothing).toBeNull();
+			expect(nothing).toBeUndefined();
 			expect(err)
 			.toEqual(new Error("Cannot resolve context parameter 'nothing'"));
-			done();
-		});
-	});
-
-	it("should propagate errors in the rules", function (done) {
-		ctx.get(function (err, errorRule) {
-			expect(errorRule).toBeNull();
-			//expect(err.message).toEqual("There was an error");
-			done();
-		});
-	});
-
-	it("should propagate errors in the rule dependencies", function (done) {
-		ctx.get(function (err, dependsOnErrorRule) {
-			expect(dependsOnErrorRule).toBeNull();
-			//expect(err.message).toEqual("There was an error");
 			done();
 		});
 	});
@@ -229,5 +269,51 @@ describe("ctx.get", function () {
 			done();
 			return errorRule + errorRule2;
 		});
+	});
+
+	it("should allow parallel gets of the same async value",
+		function (done) {
+
+		var count = 2;
+		ctx.get(function (async10) {
+			expect(async10).toEqual(10);
+			count--;
+			checkDone();
+		});
+
+		ctx.get(function (async10) {
+			expect(async10).toEqual(10);
+			count--;
+			checkDone();
+		});
+
+		function checkDone() {
+			if (count === 0) {
+				done();
+			}
+		}
+	});
+
+	it("should give error for rules that depend on non-existing parameters",
+		function (done) {
+		ctx.get(function (err, dependsOnNothing) {
+			expect(err).toEqual(new Error());
+			done();
+			return dependsOnNothing;
+		});
+	});
+});
+
+describe("ctx.keys", function () {
+	it("should return all keys within the context", function () {
+		Object.prototype.nastiestKey = "Who would do this?";
+		var ctx = fad.create([
+			function hello(cb) {
+				cb(null, "hi");
+			}
+		]);
+		ctx.set("there", 20);
+
+		expect(ctx.keys()).toEqual(["there", "hello"]);
 	});
 });
--- test/test.js
+++ test/test.js
@@ -1,24 +1,93 @@
 var fad = require("..");
 
 var rules = [
-    function async10(cb) {
-        setTimeout(function () {
-            cb(null, 10);
-        }, 20);
-    },
-    function addResult(a, b, async10, cb) {
-        cb(null, a + b + async10);
-    }
-];
+        function number20(noone, cb) {
+            cb(null, 30);
+        },
+        function number20(cb) {
+            cb(null, 20);
+        },
+        function async10(cb) {
+            setTimeout(function () {
+                cb(null, 10);
+            }, 200);
+        },
+        function addResult(a, b, async10, cb) {
+            cb(null, a + b + async10);
+        },
+        function errorRule(cb) {
+            cb(new Error("There was an error"));
+        },
+        function errorRule2(cb) {
+            cb(new Error("2nd error"));
+        },
+        function dependsOnErrorRule(errorRule, cb) {
+            cb(null, "no problem");
+        }
+    ];
 
 var ctx = fad.create(rules);
 ctx.set("a", 10);
 ctx.set("b", 20);
 
+/*
+ctx.get(4, function(err, value) {
+    console.log(err);
+});
+*/
 
+/*
+ctx.get(function (err, nothing) {
+            expect(nothing).toBeNull();
+            expect(err)
+            .toEqual(new Error("Cannot resolve context parameter 'nothing'"));
+            done();
+        });
+*/
+
+
+
+/*
+ctx.get(function (err, dependsOnErrorRule) {
+   console.log([err, dependsOnErrorRule]);
+});
+
+ctx.get(function (err, dependsOnErrorRule) {
+   console.log([err, dependsOnErrorRule]);
+});
+
+
+
+ctx.get(function (err, errorRule) {
+    console.log([err, errorRule]);
+});
+*/
+
+/*
+var count = 2;
+ctx.get(function (async10) {
+    count--;
+    checkDone();
+});
+
+ctx.get(function (async10) {
+    count--;
+    checkDone();
+});
+
+function checkDone() {
+    if (count === 0) {
+        console.log("done");
+    }
+}
+*/
+
+
+/*
 ctx.get(function (a, b) {
     console.log("--> ", [a, b]);
 });
+*/
 
 /*
 ctx.get(function (addResult) {
